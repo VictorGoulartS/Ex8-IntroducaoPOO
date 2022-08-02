879a80cde255f4a33ce29bd6c0274a7b40760363
Victor <Victor@DESKTOP-SKOFSMV>
2022-08-02 06:56:16 -0300

    Add project files.

---
+++ Exercicio8/Triangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercicio8
+{
+    class Triangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return (Largura + Altura) * 2.0;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Largura * Largura + Altura * Altura);
+        }
+    }
+}
